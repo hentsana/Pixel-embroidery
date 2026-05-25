8c21e3544e535d1888396a2b4b5c419ed30444b8
hentsa <violetta.topchiy@gmail.com>
2026-05-25 23:11:07 +0300

    refactor: StartApplication

--- src/main/java/pixelempbroidery/StartApplication.java
+++ src/main/java/pixelempbroidery/StartApplication.java
@@ -7,12 +7,13 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class StartApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("startBanner.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(StartApplication.class.getResource("startBanner.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
+        stage.setTitle("Піксельна вишивка!");
+        stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
     }
